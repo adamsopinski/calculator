2de832436e19cd2ccf74ddc6ca9d235a3e71e135
Adam Sopinski <adam.sopinski@hotmail.com>
2022-05-19 21:21:35 -0400

    Add improved functionality to Calculator. On bug where 6/4 displays 1 rather than 1.5.

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function multiply(a, b){
 }
 
 function divide(a, b){
-    return a / b;
+    let value = (a/b).toFixed(5).toString();
+    console.log(value);
+    while(value[value.length - 1] == "0"){
+        value = value.substring(0, value.length - 1);
+    }
+    console.log(value);
+    return parseInt(value);
 }
 
 function operate(a, b, operator){
@@ -19,21 +25,67 @@ function operate(a, b, operator){
 
     if (operator == "-") return subtract(a, b);
 
-    if (operator == "*") return multiply(a, b);
+    if (operator == "x") return multiply(a, b);
 
-    if (operator == "/") return divide(a, b);
+    if (operator == "÷") return divide(a, b);
 }
 //___________________________________________________________________________________
 
-totalValue = "";
+let currentOperator = "";
+let firstOperand;
+let inOperation = false;
+var text = document.getElementById('text');
+
 
 const digits = document.querySelectorAll('.num');
 
 digits.forEach((digit) => {
     digit.addEventListener('click', () =>{
-        var text = document.getElementById('text');
+        if(text.value == "x" || text.value == "+" || text.value == "÷" || text.value == "-" || text.value == "0"){
+            text.value = "";
+        }
+
+        if(inOperation){
+            firstOperand = text.value;
+            text.value = "";
+        }
+        
         text.value += digit.textContent;
-        totalValue += digit.textContent;
+        inOperation = false;
     });
 });
 
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () =>{
+
+        if(firstOperand && text.value) {
+            inOperation = true;
+            firstOperand = operate(parseInt(firstOperand), parseInt(text.value), currentOperator);
+            text.value = firstOperand;
+        }
+        else{
+            firstOperand = text.value;
+            text.value = currentOperator;
+        }
+
+        currentOperator = operator.textContent;
+    });
+});
+
+const clearButton = document.querySelector('.clear');
+
+clearButton.addEventListener('click', () => {
+    text.value = "";
+    firstOperand = 0;
+    inOperation = false;
+});
+
+const equalsButton = document.querySelector('.equals');
+
+equalsButton.addEventListener('click', () => {
+    text.value = operate(parseInt(firstOperand), parseInt(text.value), currentOperator);
+    inOperation = false;
+
+});
